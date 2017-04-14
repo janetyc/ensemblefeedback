e61e6f1ef54b3166ce3b22adf69ac3ce521daadf
janetyc <janetyc@gmail.com>
2017-04-14 10:50:50 +0800

    db migration

---
+++ migrations/versions/fab49039e1fb_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: fab49039e1fb
+Revises: None
+Create Date: 2017-04-14 10:49:54.217912
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'fab49039e1fb'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
