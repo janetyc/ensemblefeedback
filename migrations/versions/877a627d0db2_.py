ff90a1c4680a10a3f1aad995447d6f35df8b5c86
janetyc <janetyc@gmail.com>
2017-04-17 22:19:39 +0800

    db migration

---
+++ migrations/versions/877a627d0db2_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 877a627d0db2
+Revises: 127fe725032d
+Create Date: 2017-04-17 21:21:17.830674
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '877a627d0db2'
+down_revision = '127fe725032d'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
