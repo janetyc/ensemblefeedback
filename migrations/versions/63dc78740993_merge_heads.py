b7cc4db3bd6003edf336d0e3bcd7b63f0aae4893
Caroline Chen <carolinetychen@gmail.com>
2017-04-19 11:26:02 +0800

    merge db heads

---
+++ migrations/versions/63dc78740993_merge_heads.py
@@ -0,0 +1,22 @@
+"""merge heads
+
+Revision ID: 63dc78740993
+Revises: ('8310b2025b13', '151cca14453e')
+Create Date: 2017-04-19 11:24:25.154971
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '63dc78740993'
+down_revision = ('8310b2025b13', '151cca14453e')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
