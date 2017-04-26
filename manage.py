c1a99d5aaa5532ebc3c54f424d415f5b1ad1d9a2
Caroline Chen <carolinetychen@gmail.com>
2017-04-26 20:12:07 +0800

    revise init_evaluate

--- manage.py
+++ manage.py
@@ -44,8 +44,8 @@ def init_comparison():
 @manager.command
 def init_evaluate():
     # initialize evaluate db
-    start = 65
-    end = 108
+    start = 109
+    end = 110
     for i in range(start, end + 1):
         DBQuery().add_evaluate_by_revision_id(i)
 
